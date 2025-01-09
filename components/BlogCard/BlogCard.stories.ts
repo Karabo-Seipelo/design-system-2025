26d12d9e5bb82ecc405440b640090a2031041df7
Karabo Seipelo <karabo.seipelo@betika.com>
2025-01-09 22:12:45 +0200

    still working on blog card

--- components/BlogCard/BlogCard.stories.ts
+++ components/BlogCard/BlogCard.stories.ts
@@ -13,4 +13,14 @@ const meta = {
   export default meta;
   type Story = StoryObj<typeof meta>;
   
-  export const BlogCard: Story = {};
+  export const BlogCard: Story = {
+    args: {
+      imageUrl: "spacejoy-YqFz7UMm8qE-unsplash.jpg",
+      title: 'How to create a cozy living room',
+      post: 'Curated vibrants colors for your living, make it pop & calm in the same time.',
+      link: {
+        href: 'https://www.spacejoy.com',
+        label: 'Read more',
+      },
+    }
+  };
--- components/BlogCard/index.tsx
+++ components/BlogCard/index.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import Image from 'next/image';
 
-const BlogCard = () => {
+export interface BlogCardProps  {
+    imageUrl: string;
+    title: string;
+    category?: string;
+    post: string;
+    link: {
+        href: string;
+        label: string;
+    };
+}
+
+const BlogCard = ({
+    title,
+    imageUrl,
+    post,
+    link,
+} : BlogCardProps) => {
     return (
         <div className='w-[340px] flex flex-col bg-white rounded-lg'>
+            {imageUrl && <Image src={imageUrl} className="rounded-t-lg h-112" alt="Blog card" width={340} height={200} />}
             <div className="flex flex-col gap-3 self-stretch px-4 py-6">
-                <header className="flex flex-col gap-2 self-stretch">
-                    <div className="inline-flex items-center bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200 font-normal text-sm text-center text-green-70">
-                        <span className="font-normal text-sm text-center text-green-700">Interior</span>
-                    </div>
-                    <h5 className="font-semibold text-lg text-neutral-900">Top 5 Living Room Inspirations</h5>
+                <header className="flex flex-col gap-2">
+                    <ul>
+                        <li className="inline-flex bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200 font-normal text-sm text-center text-green-70">
+                            <span className="font-normal text-sm text-center text-green-700">Interior</span>
+                        </li>
+                    </ul>
+                   {title && <h5 className="font-semibold text-lg text-neutral-900">{title}</h5>}
                 </header>
-                <article className="flex flex-col gap-6 self-stretch font-medium text-base text-neutral-600">
-                Curated vibrants colors for your living, make it pop &amp; calm in the same time.
-                </article>
-                <button className="flex justify-left items-center gap-1.5 rounded font-medium text-base text-indigo-700">Read more</button>
+                {post && <article className="flex flex-col gap-6 self-stretch font-medium text-base text-neutral-600">
+                {post}
+                </article>}
+                {link &&  <button className="flex justify-left items-center gap-1.5 rounded font-medium text-base text-indigo-700">{link.label}</button>}
+               
             </div>
         </div>
     );
