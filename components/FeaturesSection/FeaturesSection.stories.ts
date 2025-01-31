8e6e4356f2fa4e669f992ef209ab72ec315e4897
Karabo Seipelo <karabo.seipelo@betika.com>
2025-01-31 14:46:27 +0200

    Add grid layout option and new features to FeaturesSection component

--- components/FeaturesSection/FeaturesSection.stories.ts
+++ components/FeaturesSection/FeaturesSection.stories.ts
@@ -65,3 +65,44 @@ export const FeatureSectionLeft: Story = {
         orientation: "left"
     }
 };
+
+export const FeatureSectionGrid: Story = {
+    args: {
+        title: "Easy access to top quality images",
+        subTitle: "Premium abstract images",
+        description: "In a world where storytelling constantly evolves, we lead with groundbreaking images designed for your presentation excellence.",
+        feautureLayout: "grid",
+        features: [
+            {
+                title: "Infinite Downloads",
+                description: "Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.",
+                icon: "download-2-line.svg"
+            },
+            {
+                title: "Purely Handcrafted",
+                description: " No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust.",
+                icon: "brush-line.svg"
+            },
+            {
+                title: "All Are Under licensed",
+                description: "The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper).",
+                icon: "copyright-line.svg"
+            },
+            {
+                title: "Cancel Anytime",
+                description: "Subscribe at your own pace, and cancel when you feel it's enough.",
+                icon: "refund-2-fill.svg"
+            },
+            {
+                title: "Empowering For Team",
+                description: "We support multiple seats at once, requiring only a single payment.",
+                icon: "team-line.svg"
+            },
+            {
+                title: "No Limitations",
+                description: "Use as many as you want, from Dribbble presentations to PowerPoint presentations.",
+                icon: "refresh-line.svg"
+            }
+        ]
+    }
+};
