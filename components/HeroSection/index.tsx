bd34646ae70ee9c3b4e89af31b022d04595470a9
Karabo Seipelo <1788640+Karabo-Seipelo@users.noreply.github.com>
2025-01-31 12:25:50 +0200

    Feature/ds 2 (#22)
    * Add FeaturesSection component with Storybook integration and SVG assets
    
    * Unexpected duplicate selector body
    
    * Refactor FeaturesSection component to accept imageUrl prop and update Storybook story
    
    * Enhance FeaturesSection component with new props for title, subtitle, description, and features; update Storybook stories accordingly
    
    * Refactor FeaturesSection component to improve icon styling and layout
    
    * Fix: Prefer using an optional chain expression instead, as it's more concise and easier to read
    
    * Refactor BlogCard and HeroSection components for improved layout and styling; adjust image handling and button styles
    
    ---------
    
    Co-authored-by: Karabo Seipelo <karabo.seipelo@betika.com>

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -20,34 +20,37 @@ const HeroSection = ({
     buttons,
     features
 } : HeroSectionProps) => {
-    const buttonStyling = (primary: boolean) => (primary ? "basis-1/2 md:basis-1/3 bg-indigo-700 py-3 rounded text-white " : "basis-1/2 md:basis-1/3 bg-white py-3 rounded border-[0.5px] border-solid border-neutral-200")
-    return (
-        <section className="container mx-auto px-4 sm:px-6 lg:p-8 lg:py-10 pt-10 md:pb-10 self-stretch bg-gradient-to-b from-gray-50 to-[#edf0f3] shadow-md rounded p-4 max-w-[320px] sm:max-w-[768px] lg:max-w-[1440px]">
-            <div className="flex flex-col lg:flex-row lg:gap-5">
-                <header className="flex items-center mb-5">
-                    <div className="align-items justify-content">
-                        <div className="">
-                            {title && <h2 className="font-semibold text-4xl text-neutral-900 w-full md:text-5xl lg:text-6xl">{title}</h2>}
-                            {description && <div className="font-normal text-lg text-neutral-600 w-full py-2.5 md:text-xl">{description}</div>}
-                            {features && <ul className="my-5">
-                                {features.map((feature, index) => (
-                                    <li key={feature + " " + index} className="flex justify-start"><Image src="check-fill.svg" width={24} height={24} alt="check" className='mr-2' />{feature}</li>
-                                ))}
-                            </ul>}
-                        </div>
-                        <div className="flex gap-4 self-stretch mt-5">
-                            {buttons.map((button, index) => (
-                                <button key={`${button.label + " " + index}`} className={buttonStyling(button.primary)} aria-label="Learn more">{button.label}</button>
-                            ))}
-                        </div>
+    const buttonStyling = (primary: boolean) => (primary ? "md:w-[213px] bg-indigo-700 py-3 rounded text-white " : "md:w-[213px] bg-white py-3 rounded border-[0.5px] border-solid border-neutral-200");
+
+    return (<div className="w-full rounded bg-white shadow-sm md:rounded-md md:shadow-md lg:shadow-lg">
+        <div className="flex h-full flex-col imtes-start px-3 py-12 md:px-4 md:py-16 lg:px-24 lg:py-24">
+            <section className="flex flex-col gap-12 md:gap-16 lg:flex-row">
+                <header className="flex flex-col items-left text-left lg:basis-1/3">
+                    {title && <h1 className="text-3xl md:text-5xl font-semibold text-neutral-900 pb-4">{title}</h1>}
+                    {description && <p className="font-normal text-lg text-neutral-600 md:text-wrap md:text-xl">{description}</p>}
+                    {features && <ul className="my-5 flex flex-col gap-5 w-full">
+                        {features.map((feature, index) => (
+                            <li key={feature + " " + index} className="flex">
+                                <Image src="check-fill.svg" width={24} height={24} alt="check" className='mr-2' />
+                                {feature}
+                            </li>
+                        ))}
+                    </ul>}
+                    <div className="flex gap-4 self-stretch mt-5">
+                        {buttons.map((button, index) => (
+                            <button key={`${button.label + " " + index}`} className={buttonStyling(button.primary)} aria-label="Learn more">{button.label}</button>
+                        ))}
                     </div>
                 </header>
-                <footer className="relative w-full max-w-xs mx-auto pb-[100%] md:max-w-2xl md:pb-[50%]">
-                    <Image src={imageUrl} alt="testing" fill className="object-contain" />
-                </footer>
-            </div>
-        </section>
-    )
+                <main className="flex flex-col gap-12 lg:flex-row lg:items-start lg:gap-8 lg:basis-2/3">
+                    <div className="flex w-full lg:flex-1">
+                        <Image src={imageUrl} alt="testing" height={0} width={0} className="object-cover flex min-h-[180px] w-full justify-center rounded-lg object-cover shadow-lg md:h-[394px] lg:h-auto
+                                lg:self-stretch 2xl:h-[450px]" />
+                    </div>
+                </main>
+            </section>
+        </div>
+    </div>);
 }
 
 export default HeroSection;
