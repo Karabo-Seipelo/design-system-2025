fc11e3fe25e5332e8f97c1955e9f05c8f91fb0fc
Karabo Seipelo <karabo.seipelo@betika.com>
2025-02-03 16:54:21 +0200

    Add PricingTable component and related Storybook configuration

---
+++ components/PricingTable/PricingTable.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PricingTable from '.';
+
+const meta = {
+    title: 'Components/PricingTable',
+    component: PricingTable,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof PricingTable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Single: Story = {
+    args: {
+        title: 'Pay as you need',
+        description: 'We offer one-time purchases with credits, for you to use as needed. Always active.',
+        subTitle: 'One time purchase',
+        featureTitle: "Unlock creativity once, enjoy forever",
+        features: [
+            "128 available credits for all images",
+            "Up to 3 users",
+            "24 hour response time",
+            "Advanced analytics"
+        ],
+        tag: "Popular",
+        buttons: [
+            {
+                label: "Buy now",
+                primary: true
+            }
+        ]
+    }
+};
