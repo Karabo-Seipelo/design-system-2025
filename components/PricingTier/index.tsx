e944629f6c47e27578af64bb4a5ef3f9b7a7634f
Karabo Seipelo <1788640+Karabo-Seipelo@users.noreply.github.com>
2025-02-04 18:38:10 +0200

    Feature/ds 14 (#33)
    * Add PricingTier component and update PricingTable to support multiple prices
    
    * Refactor PricingTier component layout for improved responsiveness and styling
    
    * Add documentation for Pricing and PricingTier components, including features, props, usage, and customization guidelines
    
    * Enhance Pricing components with improved styling and functionality, including highlight feature for pricing tiers and responsive layout adjustments
    
    * Refactor PricingTier stories to use plan factory functions for improved readability and maintainability

--- components/PricingTier/index.tsx
+++ components/PricingTier/index.tsx
@@ -1,50 +1,49 @@
 import Image from 'next/image';
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 
-interface Button {
+type Button = {
     label: string;
     primary: boolean;
 }
 
-interface Price {
+type Price = {
+    highlight?: boolean;
     tag?: string;
     classes?: string;
     plan: string;
-    price: string,
-    billedAt: string,
-    description: string,
-    buttons: Button[],
+    price: string;
+    billedAt: string;
+    description: string;
+    buttons: Button[];
     features?: string[];
-}
+};
 
-interface Tiers {
+type Tiers = {
     subscription: string;
     prices: Price[];
-}
+};
 
-interface PricingProps {
+type PricingProps = {
     title: string;
     subTitle: string;
     description?: string;
     featureTitle: string;
     features?: string[];
     tiers?: Tiers[];
-}
+};
 
 
-const Price = ({ tag, price, description, features, buttons, classes, plan} : Price) => {
+const Price = ({ tag, price, description, features, buttons, classes, plan, billedAt, highlight = false} : Price) => {
     const buttonStyling = (primary: boolean) => (primary ? "w-full bg-indigo-700 py-3 rounded text-white " : "w-full bg-white py-3 rounded border-[0.5px] border-solid border-neutral-200");
     return (
-        <div className={`flex flex-col gap-y-8 w-full bg-white border rounded-lg ${classes}`}>
-            <div className="flex h-full flex-col gap-10 px-3 py-12  md:px-4 md:py-16 lg:justify-center">
-                <div className="flex flex-col text-center gap-2">
-                    {tag && <div className="bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200 font-normal text-sm text-center text-green-70 mr-1">
-                        <span className="font-normal text-sm text-center text-green-700">{tag}</span>
-                    </div>}
+        <div className={`flex flex-col gap-y-8 w-full bg-white border-solid rounded-lg ${classes} ${highlight ? "border-2 border-solid border-indigo-700 shadow-[0_25px_50px_rgba(0,0,0,0.25)]" : "border border-solid border-neutral-200"}`}>
+           {highlight && tag && <div className="bg-indigo-50 py-4 rounded-tl-lg rounded-tr-lg items-center self-stretch font-bold text-xl text-center text-indigo-700">{tag}</div>}
+           <div className={`flex flex-col gap-8 h-full ${highlight ? "px-8 pb-8" : "p-8"}`}>
+                <div className="flex flex-col text-left gap-4">
                     {plan && <h3 className="font-semibold text-2xl text-neutral-900">{plan}</h3>}
-                    <p className="font-normal text-sm text-center text-neutral-600">{description}</p>
-                    <h2 className="font-semibold text-5xl text-center text-neutral-900">{price}</h2>
-                   
+                    <p className="font-normal text-sm text-neutral-600">{description}</p>
+                    <h2 className={`font-semibold text-5xl ${highlight ? "text-indigo-700" : "text-neutral-900"}`}>{price}</h2>
+                    <small>{billedAt}</small>
                 </div>
                 {features && <ul className="my-5 flex flex-col gap-5 w-full">
                     {features.map((feature, index) => (
@@ -54,12 +53,12 @@ const Price = ({ tag, price, description, features, buttons, classes, plan} : Pr
                         </li>
                     ))}
                 </ul>}
-                <div className="flex flex-col w-full">
+                <div className="flex flex-col w-full mt-auto">
                     {buttons.map((button, index) => (
                         <button key={`${button.label + " " + index}`} className={buttonStyling(button.primary)} aria-label="Learn more">{button.label}</button>
                     ))}
                 </div>
-            </div>
+           </div>
         </div>
     )
 }
@@ -81,7 +80,7 @@ const Pricing = ({
     
     return (
         <div className="w-full rounded bg-white shadow-sm container-karabo">
-            <div className="flex h-full gap-15 lg:gap-20 flex-col px-3 py-12 md:px-4 md:py-16 lg:p-24">
+            <div className="flex h-full gap-10 lg:gap-20 flex-col px-3 py-12 md:px-4 md:py-16 lg:p-24">
                 <section className="flex flex-col gap-12 md:gap-16">
                     <header className="flex flex-col items-center justify-center text-center">
                         {subTitle && <small className="text-base font-semibold text-indigo-700 pb-4">{subTitle}</small>}
@@ -89,26 +88,22 @@ const Pricing = ({
                         {description && <p className="font-normal text-lg md:text-xl text-neutral-600">{description}</p>}
                     </header>
                 </section>
-                <main className="flex flex-col gap-5">
-                    <div className="flex flex-col w-full place-items-stretch">
-                        <TabGroup className="flex lg:flex-col gap-5 lg:gap-10">
-                            <TabList className="flex items-center gap-4 self-stretch outline-none">
-                                {tiers && tiers.map((tier, index) => {
-                                    return <Tab key={`tier-${index}`} className="justify-center items-center gap-1.5 grow px-4 py-2.5 rounded border-solid border-neutral-200 bg-white data-[selected]:border-[0.5px]">{tier.subscription}</Tab>
+                <TabGroup className="flex flex-col gap-5 lg:gap-10">
+                    <TabList className="flex flex-row items-center gap-4 self-stretch outline-none w-full md:w-[280px] md:m-auto">
+                        {tiers && tiers.map((tier, index) => {
+                            return <Tab key={`tier-${index}`} className="justify-center items-center gap-1.5 grow px-4 py-2.5 rounded border-solid border-neutral-200 bg-white data-[selected]:border-[0.5px]">{tier.subscription}</Tab>
+                        })}
+                    </TabList>
+                    <TabPanels className="flex flex-row gap-5 md:w-full">
+                        {tiers && tiers.map((tier, index) => {
+                            return <TabPanel key={`tier-${index}`} className="flex flex-col w-full lg:flex-row gap-y-8 lg:gap-x-8 mt-12">
+                                {tier.prices.map((price, index) => {
+                                        return <Price key={`price-${index}`} {...price} />
                                 })}
-                            </TabList>
-                            <TabPanels className="flex flex-row gap-5">
-                                {tiers && tiers.map((tier, index) => {
-                                    return <TabPanel key={`tier-${index}`} className="flex flex-col lg:flex-row gap-y-8 lg:gap-x-8 mt-12">
-                                        {tier.prices.map((price, index) => {
-                                                return <Price key={`price-${index}`} {...price} />
-                                        })}
-                                    </TabPanel>
-                                })}
-                            </TabPanels>
-                        </TabGroup>
-                    </div>
-                </main>
+                            </TabPanel>
+                        })}
+                    </TabPanels>
+                </TabGroup>
             </div>
         </div>
     )
