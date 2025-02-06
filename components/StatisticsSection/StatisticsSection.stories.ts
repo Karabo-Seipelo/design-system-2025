8a01ae59824d91af5b83a5f64ffe287a3e438d00
Karabo Seipelo <karabo.seipelo@betika.com>
2025-02-06 08:48:10 +0200

    Add StatisticsSection component with Storybook stories

---
+++ components/StatisticsSection/StatisticsSection.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import StatisticsSection from '.';
+
+const meta = {
+    title: 'Components/StatisticsSection',
+    component: StatisticsSection,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof StatisticsSection>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        title: 'More than premium abstract imagery',
+        subTitle: 'Statistics',
+        description: 'Our platform is more than just as a service to us - it is a catalyst for enriching lives through premium abstract imagery.',
+        imageUrl: "white-blocks.png",
+        statisticsTitle: "Our mission, in numbers",
+        statistics: [
+            {
+                statistic: "25,664,890",
+                label: "Downloads"
+            },
+            {
+                statistic: "17,219",
+                label: "Paid users"
+            },
+            {
+                statistic: "190,654,321",
+                label: "Images in library"
+            }
+        ]
+    },
+}
