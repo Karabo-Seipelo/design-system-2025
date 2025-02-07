8f18a968837045363146342f04fe82f1168f19f8
Karabo Seipelo <1788640+Karabo-Seipelo@users.noreply.github.com>
2025-02-07 16:16:45 +0200

    Add NewsletterSection and List components with Storybook stories (#38)
    * Add NewsletterSection and List components with Storybook stories
    
    * Refactor List and NewsletterSection components for improved styling and functionality
    
    * Enhance NewsletterSection and List components with responsive design and image support
    
    ---------
    
    Co-authored-by: Karabo Seipelo <karabo.seipelo@betika.com>

---
+++ components/NewsletterSection/NewsletterSection.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import NewsletterSection from '.';
+
+const meta = {
+    title: 'Components/Newsletter',
+    component: NewsletterSection,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof NewsletterSection>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        title: "Get the finest curated abstracts delivered weekly to your inbox",
+        features: [
+            {
+                description: "Exclusive access to new abstract images and collections",
+                icon: "check-fill.svg"
+            },
+            {
+                description: "Unlock special promotions only for subscribers",
+                icon: "check-fill.svg"
+            },
+            {
+                description: "Regular doses of artistic inspiration",
+                icon: "check-fill.svg"
+            }
+        ],
+        imageUrl: "abstract.jpg"
+    }
+};
