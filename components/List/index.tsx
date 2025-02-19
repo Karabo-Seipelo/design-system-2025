bf27fd7a483681b712e7d97ed2c6131c184b21be
Karabo Seipelo <karabosouldier@gmail.com>
2025-02-19 18:49:37 +0200

    Add ContactSection component with form and contact details

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -4,14 +4,15 @@ interface Feature {
     title?: string;
     description: string;
     icon: string;
+    dropShadow?: boolean;
 }
 
-const List = ({ features }: { features: Feature[] }) => {
+const List = ({ features, dropShadow = false}: { features: Feature[], dropShadow?: boolean }) => {
     return (<ul className="flex flex-col gap-10 w-full md:gap-5">
         {features.map((feature, index) => {
             const {title, description, icon } = feature;
             return (<li className="flex flex-row gap-4 md:gap-5" key={feature.title + " " + index}>
-                    <div className="h-fit rounded-full p-3">
+                    <div className={`h-fit rounded-full p-3 ${dropShadow ? "bg-white shadow-[0_1px_3px_rgba(0,0,0,0.1)]" : ""}`}>
                         <Image src={icon} alt="icon" width={0} height={0} className="min-w-[24px] md:w-full min-h-[24px] lg:h-[24px]" />
                     </div>
                     <div className="flex flex-col items-start justify-center gap-2 py-2.5">
