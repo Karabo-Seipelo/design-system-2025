32446cb8a9e76f37c04e774c1032d62030f08d2c
Karabo Seipelo <karabosouldier@gmail.com>
2025-03-04 17:49:00 +0200

    Implement CookieConsent component with enhanced modal functionality and cookie management and only missing the save functionality

--- components/Modal/CookieConsent/CookieConsent.stories.tsx
+++ components/Modal/CookieConsent/CookieConsent.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 import CookieConsent from ".";
 
 const meta = {
@@ -16,7 +15,57 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    isOpen: true,
-    setIsOpen: fn(),
+    title: "We use cookies",
+    description:
+      "We use cookies to enhance your browsing experience and improve our website's performance. By continuing to use this site, you consent to the use of cookies. To learn more about how we use cookies and your options, please read our cookie policy.",
+    modal: {
+      cookies: [
+        {
+          name: "essential",
+          description:
+            "These cookies are essential for the proper functioning of our services and cannot be disabled.",
+        },
+        {
+          name: "analytics",
+          description:
+            "These cookies collect information about how you use our services or potential errors you encounter. Based on this information we are able to improve your experience and react to any issues.",
+        },
+        {
+          name: "marketing",
+          description:
+            "These cookies allow us to show you advertisements relevant to you through our advertising partners.",
+        },
+      ],
+      buttons: [
+        {
+          label: "Accept all",
+          type: "acceptModal",
+          groupWithNext: true,
+        },
+        {
+          label: "Save",
+          type: "saveModal",
+        },
+        {
+          label: "Decline all",
+          type: "declineModal",
+        },
+      ],
+    },
+    buttons: [
+      {
+        label: "Decline all",
+        type: "decline",
+      },
+      {
+        label: "Allow cookies",
+        type: "accept",
+        groupWithNext: true,
+      },
+      {
+        label: "Manage cookies",
+        type: "manage",
+      },
+    ],
   },
 };
