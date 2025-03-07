72e041d7c4624533dbb2672a698daa7d3a9815fd
Karabo Seipelo <karabosouldier@gmail.com>
2025-03-07 09:48:38 +0200

    Fix typo in MobileMenuDialog component className for correct positioning

--- components/NavBar/MobileMenuDialog.tsx
+++ components/NavBar/MobileMenuDialog.tsx
@@ -22,7 +22,7 @@ const MobileMenuDialog: React.FC<MobileMenuDialogProps> = ({
 }) => {
   return (
     <Dialog open={open} onClose={onClose} className="lg:hidden">
-      <div className="fixed insert-0 z-10">
+      <div className="fixed inset-0 z-10">
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 h-full flex flex-col">
           <div className="flex items-center justify-between">
             <NavLogo {...brand} />
