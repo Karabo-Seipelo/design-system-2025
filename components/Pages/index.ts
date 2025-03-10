bb51c78ee04e41f51329d58f4fff3a4685bb536f
Karabo Seipelo <karabosouldier@gmail.com>
2025-03-10 22:51:18 +0200

    Update import paths to use absolute paths in Pages index file

--- components/Pages/index.ts
+++ components/Pages/index.ts
@@ -1,14 +1,14 @@
-import { NavItem, Brand } from "../NavBar/index";
-import { CallToAction } from "../NavBar/CallToAction";
-import { HeroSectionProps } from "../Section/Hero";
-import { FeatureSectionProps } from "../Section/Features/index";
-import { FAQProps } from "../Section/Faq";
-import { FooterProps } from "../Section/Footer";
-import { ContactSectionProps } from "../Section/Contact/interfaces";
-import { TestimonialsProps } from "../Section/Testimonials";
-import { NewsletterSectionProps } from "../Section/Newsletter";
-import { LogoMarqueeProps } from "../LogoMarquee/index";
-import { PricingProps } from "../Section/PricingTable/Tier";
+import { NavItem, Brand } from "$/NavBar/index";
+import { CallToAction } from "$/NavBar/CallToAction";
+import { HeroSectionProps } from "$/Section/Hero";
+import { FeatureSectionProps } from "$/Section/Features/index";
+import { FAQProps } from "$/Section/Faq";
+import { FooterProps } from "$/Section/Footer";
+import { ContactSectionProps } from "$/Section/Contact/interfaces";
+import { TestimonialsProps } from "$/Section/Testimonials";
+import { NewsletterSectionProps } from "$/Section/Newsletter";
+import { LogoMarqueeProps } from "$/LogoMarquee/index";
+import { PricingProps } from "$/Section/PricingTable/Tier";
 
 export interface Navigation {
   navItems: NavItem[];
