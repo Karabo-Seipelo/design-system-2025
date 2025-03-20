0e86fec354e32d83e22d526d9abedfd6e0c37f2f
Karabo Seipelo <karabosouldier@gmail.com>
2025-03-20 12:53:41 +0200

    Update import paths for Artboard component to use absolute paths

--- components/organisms/404/NotFound.stories.tsx
+++ components/organisms/404/NotFound.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import Artboard from "../../atoms/artboard";
+import Artboard from "$/atoms/artboard";
 import NotFound from ".";
 
 const meta = {
