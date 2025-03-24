54f9a1cfc99661f5c9df574b5d1a5f7a5123ee20
Karabo Seipelo <karabosouldier@gmail.com>
2025-03-24 23:30:56 +0200

    Enhance Toast component stories with fullscreen layout and interactive toast display functionality

--- components/organisms/Toast/Toast.stories.tsx
+++ components/organisms/Toast/Toast.stories.tsx
@@ -1,13 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Toast from ".";
+import useToast from "./useToast";
 
 const meta = {
   title: "Component/Feedback/Toast",
   component: Toast,
   tags: ["autodocs"],
   parameters: {
-    layout: "centered",
+    layout: "fullscreen",
   },
+  decorators: [
+    (Story) => (
+      <div className="w-full min-h-screen bg-gray-100">
+        <div className="flex h-full flex-col items-start lg:items-center lg:justify-center py-[264px] px-[16px] md:py-[356px] md:px-[8px] lg:py-[196px] lg:px-[8px]">
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof Toast>;
 
 export default meta;
@@ -19,6 +29,21 @@ export const SuccessToast: Story = {
     message: "Subscription successful! Please check your email to confirm.",
     status: "SUCCESS",
   },
+  render: (args) => {
+    const { showToast } = useToast();
+    const { badge, message, status } = args;
+    return (
+      <>
+        <Toast {...args} />
+        <button
+          onClick={() => showToast(message, status, badge)}
+          className="flex justify-center items-center gap-1.5 px-4 py-2.5 rounded bg-indigo-700 text-white"
+        >
+          Show Toast
+        </button>
+      </>
+    );
+  },
 };
 
 export const ErrorToast: Story = {
@@ -26,5 +51,20 @@ export const ErrorToast: Story = {
     badge: "Error",
     message: "Subscription failed. Please try again.",
     status: "ERROR",
+  },
+  render: (args) => {
+    const { showToast } = useToast();
+    const { badge, message, status } = args;
+    return (
+      <>
+        <Toast {...args} />
+        <button
+          onClick={() => showToast(message, status, badge)}
+          className="flex justify-center items-center gap-1.5 px-4 py-2.5 rounded bg-indigo-700 text-white"
+        >
+          Show Toast
+        </button>
+      </>
+    );
   },
 };
