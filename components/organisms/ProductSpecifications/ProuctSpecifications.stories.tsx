fbdf84e7df8a2d590969b2f49681260cbfe4860e
Karabo Seipelo <karabosouldier@gmail.com>
2025-04-22 15:40:08 +0200

    Add specification data for ProductSpecifications component

--- components/organisms/ProductSpecifications/ProuctSpecifications.stories.tsx
+++ components/organisms/ProductSpecifications/ProuctSpecifications.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Artboard from "../../atoms/Artboard/index";
 import ProductSpecifications from "./";
+import Specification from "./specification.json";
 
 const meta: Meta<typeof ProductSpecifications> = {
   title: "E-commerce/ProductSpecifications",
@@ -41,60 +42,16 @@ const createSpecificantion = (
   list,
 });
 
-const specifications = [
+const specifications = Specification[0].specifications.map((specification) =>
   createSpecificantion(
-    "Sustainability",
-    "Eco-Friendly Choice",
-    "With our sustainable approach, we curate clothing that makes a statement of care—care for the planet, and for the art of fashion.",
-    "product-img-main.jpg",
-    "Placeholder Image 1",
-    [
-      { icon: "recycle", text: "Recycled Materials" },
-      { icon: "paint", text: "Low Impact Dye" },
-      { icon: "plant", text: "Carbon Neutral" },
-      { icon: "water", text: "Water Conservation" },
-    ]
-  ),
-  createSpecificantion(
-    "Comfort",
-    "Uncompromised Comfort",
-    "Our garments are a sanctuary of softness, tailored to drape gracefully and allow for freedom of movement.",
-    "product-img-main2.jpg",
-    "Placeholder Image 2",
-    [
-      { icon: "t-shirt", text: "Ergonomic Fits" },
-      { icon: "hand-heart", text: "Soft-to-the-Touch Fabrics" },
-      { icon: "windy", text: "Breathable Weaves" },
-      { icon: "color-filter", text: "Thoughtful Design" },
-    ]
-  ),
-  createSpecificantion(
-    "Durability",
-    "Built to Last",
-    "Here’s to apparel that you can trust to look as good as new, wear after wear, year after year.",
-    "product-img-main3.jpg",
-    "Placeholder Image 3",
-    [
-      { icon: "stack", text: "Reinforced Construction" },
-      { icon: "scales", text: "Quality Control" },
-      { icon: "shield-star", text: "Material Resilience" },
-      { icon: "price-tag-2", text: "Warranty and Repair" },
-    ]
-  ),
-  createSpecificantion(
-    "Versatility",
-    "Versatile by Design",
-    "Our pieces are a celebration of versatility, offering a range of styles that are as perfect for a business meeting as they are for a casual brunch.",
-    "product-img-main4.jpg",
-    "Placeholder Image 4",
-    [
-      { icon: "rainbow", text: "Adaptive Styles" },
-      { icon: "shirt", text: "Functional Fashion" },
-      { icon: "plant", text: "Timeless Aesthetics" },
-      { icon: "shapes", text: "Mix-and-Match Potential" },
-    ]
-  ),
-];
+    specification.name,
+    specification.title,
+    specification.description,
+    specification.image.src,
+    specification.image.alt,
+    specification.list
+  )
+);
 
 export const Default: Story = {
   args: {
