9083b27d79b511d52b72b79c480eff411657f34b
Karabo Seipelo <karabosouldier@gmail.com>
2025-04-22 15:26:35 +0200

    Fix syntax issues in createSpecification function and update placeholder image names in ProductSpecifications stories

--- components/organisms/ProductSpecifications/ProuctSpecifications.stories.tsx
+++ components/organisms/ProductSpecifications/ProuctSpecifications.stories.tsx
@@ -29,7 +29,7 @@ const createSpecificantion = (
   list: {
     icon: string;
     text: string;
-  }[],
+  }[]
 ) => ({
   name,
   title,
@@ -53,7 +53,7 @@ const specifications = [
       { icon: "paint", text: "Low Impact Dye" },
       { icon: "plant", text: "Carbon Neutral" },
       { icon: "water", text: "Water Conservation" },
-    ],
+    ]
   ),
   createSpecificantion(
     "Comfort",
@@ -66,33 +66,33 @@ const specifications = [
       { icon: "hand-heart", text: "Soft-to-the-Touch Fabrics" },
       { icon: "windy", text: "Breathable Weaves" },
       { icon: "color-filter", text: "Thoughtful Design" },
-    ],
+    ]
   ),
   createSpecificantion(
     "Durability",
     "Built to Last",
     "Here’s to apparel that you can trust to look as good as new, wear after wear, year after year.",
     "product-img-main3.jpg",
-    "Placeholder Image 2",
+    "Placeholder Image 3",
     [
       { icon: "stack", text: "Reinforced Construction" },
       { icon: "scales", text: "Quality Control" },
       { icon: "shield-star", text: "Material Resilience" },
       { icon: "price-tag-2", text: "Warranty and Repair" },
-    ],
+    ]
   ),
   createSpecificantion(
     "Versatility",
     "Versatile by Design",
     "Our pieces are a celebration of versatility, offering a range of styles that are as perfect for a business meeting as they are for a casual brunch.",
     "product-img-main4.jpg",
-    "Placeholder Image 2",
+    "Placeholder Image 4",
     [
       { icon: "rainbow", text: "Adaptive Styles" },
       { icon: "shirt", text: "Functional Fashion" },
       { icon: "plant", text: "Timeless Aesthetics" },
       { icon: "shapes", text: "Mix-and-Match Potential" },
-    ],
+    ]
   ),
 ];
 
