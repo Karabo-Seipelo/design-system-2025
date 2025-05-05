fcfb625375ff2663dee4c33457d975d7d32e70e4
Karabo Seipelo <karabosouldier@gmail.com>
2025-05-05 09:56:59 +0200

    Refactor Avatar component; make imageUrl prop optional and add fallback image

--- components/atoms/Avatar/Avatar.test.tsx
+++ components/atoms/Avatar/Avatar.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Avatar from ".";
-import { describe } from "node:test";
 
 jest.mock("next/image", () => ({
   __esModule: true,
@@ -14,10 +13,37 @@ jest.mock("next/image", () => ({
   },
 }));
 
-describe("Avatar", () => {
-  it("renders with default props", () => {
-    render(<Avatar alt="John Doe" imageUrl="/src/test.png" />);
-    const avatar = screen.getByAltText(/John Doe/i);
-    expect(avatar).toBeInTheDocument();
+describe("Avatar Component", () => {
+  describe("Given the Avatar component is rendered", () => {
+    it("should render with default props", () => {
+      render(<Avatar alt="John Doe" imageUrl="/src/test.png" />);
+      const avatar = screen.getByAltText(/John Doe/i);
+      expect(avatar).toBeInTheDocument();
+    });
+
+    it("should render the correct image URL", () => {
+      render(<Avatar alt="Jane Doe" imageUrl="/src/jane.png" />);
+      const avatar = screen.getByAltText(/Jane Doe/i) as HTMLImageElement;
+      expect(avatar.src).toContain("/src/jane.png");
+    });
+
+    it("should apply additional class names if provided", () => {
+      render(
+        <Avatar
+          alt="John Doe"
+          imageUrl="/src/test.png"
+          classes="custom-class"
+        />,
+      );
+      const avatar = screen.getByAltText(/John Doe/i);
+      expect(avatar).toHaveClass("custom-class");
+    });
+
+    it("should render a fallback if no image URL is provided", () => {
+      render(<Avatar alt="Fallback User" />);
+      const avatar = screen.getByAltText(/Fallback User/i);
+      expect(avatar).toBeInTheDocument();
+      expect(avatar.tagName).toBe("IMG");
+    });
   });
 });
